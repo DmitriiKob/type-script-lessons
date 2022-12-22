e0bec9e7966ccf0de41167aede1c1a7312eee7b9
DmitriiKob <dmitriykob1002@gmail.com>
2022-12-22 15:51:58 -0800

    lesson2

--- index.js
+++ index.js
@@ -18,3 +18,55 @@ function func(c, b) {
     return c + b;
 }
 console.log(func(1, 'lorem'));
+
+var cherchil = {
+    hairColor: "bold",
+    height: 167,
+    weight: 62,
+    age: 148,
+    nationality: "british",
+    name: "Winston",
+    surname: "Churchill",
+    country: "United Kingdom",
+    numberoOfChlidren: 5,
+    children: [{
+            name: "Diana",
+            dateOfBirth: 1909,
+            hairColor: "black",
+            height: 167,
+            weight: 62
+        },
+        {
+            name: "Sarah",
+            dateOfBirth: 1874,
+            height: 167,
+            weight: 62
+        }]
+};
+var tesla = {
+    model: "x",
+    weight: 1300,
+    yearOfRelease: 2022,
+    type: "Tesla-Original",
+    color: "black",
+    doorType: [{
+            model: "x",
+            weight: 75,
+            yearOfRelease: 2022,
+            doorColor: "yellow"
+        }],
+    wheel: [{
+            model: "x",
+            weight: 75,
+            yearOfRelease: 2022,
+            radius: 22,
+            brand: "Tesla"
+        }],
+    transmission: [{
+            model: "x",
+            weight: 75,
+            yearOfRelease: 2022,
+            country: "USA",
+            name: "Automatic Tesla Turbo Giper Super Transmission with one hair from Ilon's Mask head on top"
+        }]
+};
--- index.ts
+++ index.ts
@@ -16,3 +16,105 @@ function func(c:number, b:string): string{
 return c + b
 }
 console.log(func(1, 'lorem'))
+
+interface Human {
+    height: number
+    weight: number
+    hairColor?: string
+}
+
+interface Person extends Human{
+    name: string
+    surname: string
+    country: string
+    numberoOfChlidren: number
+    age: number
+    nationality: string
+    children: Children[]
+}
+
+interface Children extends Human{
+    name: string
+    dateOfBirth: number
+}
+
+const cherchil:Person = {
+    hairColor: "bold",
+    height: 167,
+    weight: 62,
+    age: 148,
+    nationality: "british",
+    name: "Winston",
+    surname: "Churchill",
+    country: "United Kingdom",
+    numberoOfChlidren: 5,
+    children:[{
+        name: "Diana",
+        dateOfBirth: 1909,
+        hairColor: "black",
+        height: 167,
+        weight: 62,
+    },
+    {
+       name: "Sarah",
+        dateOfBirth: 1874,
+        height: 167,
+        weight: 62
+    }]
+}
+
+interface car{
+    model: string
+    weight: number
+    yearOfRelease: number 
+}
+
+interface panel extends car{
+    type: string
+    color: string
+    doorType: door[]
+    wheel: wheel[]
+    transmission: transmission[]
+}
+
+interface door extends car{
+    doorColor: string
+}
+
+interface wheel extends car{
+    radius: number
+    brand: string
+}
+
+interface transmission extends car{
+    country: string
+    name: string
+}
+
+const tesla:panel = {
+    model: "x",
+    weight: 1300,
+    yearOfRelease: 2022 ,
+    type: "Tesla-Original",
+    color: "black",
+    doorType:[{
+        model: "x",
+        weight: 75,
+        yearOfRelease: 2022,
+        doorColor: "yellow"
+    }],
+    wheel:[{
+        model: "x",
+        weight: 75,
+        yearOfRelease: 2022,
+        radius: 22,
+        brand: "Tesla"
+    }],
+    transmission:[{
+        model: "x",
+        weight: 75,
+        yearOfRelease: 2022,
+        country:"USA",
+        name:"Automatic Tesla Turbo Giper Super Transmission with one hair from Ilon's Mask head on top"
+    }]
+}
